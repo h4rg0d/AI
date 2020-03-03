11bd5b50a74337cd7e5b6ed9a5291ce4e8e850eb
Hargod <badiisbro@gmail.com>
2020-03-03 21:10:09 +0400

    edit

--- include/neiron.h
+++ include/neiron.h
@@ -2,8 +2,7 @@
 #include "mathN.h"
 
 void forWards    (int Ni[NISIZE],int No[NOSIZE], int W[XSIZE,YSIZE],int NISIZE,int NOSIZE, int XSIZE,int YSIZE); //
-void fixOutError (); // Записываем ошибку в выходном слое
+void fixOutError (); // Write the error in the output layer
 void findError   (); //
 void backWards   (); //
-void fillW       (); // Заполнение массива весов случайными чмслами в диапозоне -0,5 .. 0,5
-
+void fillW       (); // Filling the array of weights with random numbers in the range -0.5 .. 0.5
